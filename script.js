d1fc432eed920024e2faa6def5aa8ad36fe36e67
John Manouse <johnmanouse@Macintosh-2.local>
2020-06-22 11:50:57 -0400

    create variables

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 // JQuery Document Ready Function
-$(function() {});
-
-// Variable - Current Day-Month-Date-Year in Jumbotron
-var currentDay = moment().format("dddd, MMMM Do YYYY");
-
-// Variable - Timeblocks for Day
-var hour = moment().format("H A");
+$(function () {
+    // Variable - Current Day-Month-Date-Year in Jumbotron
+    const currentDay = moment().format("dddd, MMMM Do YYYY");
+    
+    // Variable - Timeblocks for Day
+    const hour = moment().format("H A");
+​
+    // DONE - Fill the '#currentDay' container with the currentDay variable
+    $("#currentDay").text(currentDay)
+​
+    // DONE - Retrieve stored tasks and parse from local storage
+    const tasks = JSON.parse(
+        localStorage.getItem("tasks")
+    )
+​
+    // TODO - Create timeblocks and populate with any existing stored task and color appropriately based on current time
+    // ------------------------------------------------------------------------
+​
+    // TODO - Run a for-loop and create timeblocks for every hour from 9 - 5
+    const startHour = 9;
+    for (let i = 0; i < 9; i++) {
+        // The hour of this timeblock
+        const timeBlockHour = startHour + i;
+​
+        // TODO - Contains current time, textarea for input (given the .past, .present, .future as appropriate), and the save button (.saveButton)
+        const timeBlockTemplate = `<p>${timeBlockHour}</p>`
+​
+        // DONE - Put timeblocks into the container
+        $("#timeblock-container").append(timeBlockTemplate)
+​
+    }
+​
+    // TODO - Add a click event istener to all save buttons to stringify and store the entered task into local storage
+    $(".saveButton").on("click", function () {
+        // TODO - Get current value from 'this' timeblock's textarea element
+​
+        // TODO - Push the task into the 'tasks' array
+        tasks.push()
+​
+        // TODO - Store the 'tasks' array into local storage
+    })
+});
