9f4f9f1d271c03b9cb396eb61b97fe827b6038ef
John Manouse <johnmanouse@Macintosh-2.local>
2020-06-22 08:30:53 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// JQuery Document Ready Function
+$(function() {});
+
+// Variable - Current Day-Month-Date-Year in Jumbotron
+var currentDay = moment().format("dddd, MMMM Do YYYY");
+
+// Variable - Timeblocks for Day
+var hour = moment().format("H A");
